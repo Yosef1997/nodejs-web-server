a25e0f58b15eaa6a52300c5886e3b615629627bf
unknown <yosefsitumorang97@gmail.com>
2021-06-07 00:13:57 +0700

    clean code

--- server.js
+++ server.js
@@ -51,29 +51,6 @@ const requestListener = (request, response) => {
       message: 'halaman tidak ditemukan</h1>'
     }))
   }
-
-  // if(method==='GET') {
-  //   response.end('<h1>Halo GET</h1>')
-  // }
-  // if(method==='POST') {
-  //   let body = []
-
-  //     request.on('data', (chunk) => {
-  //       body.push(chunk)
-  //     })
-
-  //     request.on('end',()=> {
-  //       body = Buffer.concat(body).toString()
-  //       const {name} = JSON.parse(body)
-  //       response.end(`<h1>Hai, ${name}!</h1>`)
-  //     })
-  // }
-  // if(method==='PUT') {
-  //   response.end('<h1>Halo PUT</h1>')
-  // }
-  // if(method==='DELETE') {
-  //   response.end('<h1>Halo DELETE</h1>')
-  // }
 }
 
 const server = http.createServer(requestListener)
